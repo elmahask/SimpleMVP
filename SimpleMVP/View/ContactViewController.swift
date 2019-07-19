045f5af48e0f8def8446770f3c0c6048d16cd7fa
Wael Elmahask <welmahask@gmail.com>
2019-07-19 10:28:35 -0700

    some changes to separate protocols

--- SimpleMVP/View/ContactViewController.swift
+++ SimpleMVP/View/ContactViewController.swift
@@ -8,16 +8,12 @@
 
 import UIKit
 
-class ContactViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, ContactViewDelegate {
+class ContactViewController: UIViewController {
    
     @IBOutlet weak var diplayLabel: UILabel!
     @IBOutlet weak var tableView: UITableView!
     private let contactPresenter = ContactPresenter(providerDataService: ProviderDataService())
     private var userList = [User]()
-    
-    override func viewWillAppear(_ animated: Bool) {
-        
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +22,9 @@ class ContactViewController: UIViewController, UITableViewDataSource, UITableVie
         contactPresenter.setViewDelegate(contactViewDelegate: self)
         contactPresenter.userList()
     }
+}
+
+extension ContactViewController : UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return userList.count
@@ -36,10 +35,16 @@ class ContactViewController: UIViewController, UITableViewDataSource, UITableVie
         cell.textLabel?.text = self.userList[indexPath.row].userName
         return cell
     }
+}
+
+extension ContactViewController : UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         contactPresenter.userNameSelected(userName: userList[indexPath.row].userName)
     }
+}
+
+extension ContactViewController : ContactViewDelegate {
     
     func displayContactList(userList: [User]) {
         print("view \(userList.count)")
@@ -51,16 +56,4 @@ class ContactViewController: UIViewController, UITableViewDataSource, UITableVie
         print(phoneNumber)
         self.diplayLabel.text = phoneNumber
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
